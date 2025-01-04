924c88115844457da2c9f72311dc40eb0f677413
Eli Slade <elislade@gmail.com>
2025-01-04 00:40:17 -0800

    Improved InsetReader

--- Sources/SwiftUIKitCore/Scene/InsetReader.swift
+++ Sources/SwiftUIKitCore/Scene/InsetReader.swift
@@ -3,26 +3,33 @@ import SwiftUI
 
 public struct InsetReader<Content: View>: View {
     
+    @Environment(\.insetReadingDisabled) private var disabled
     @Environment(\.sceneInsets) private var sceneInsets
     @Environment(\.sceneSize) private var sceneSize
+    @State private var frame = CGRect()
+    @State private var disabledInsets: EdgeInsets?
     
-    private let content: (EdgeInsets) -> Content
+    private let content: @MainActor (EdgeInsets) -> Content
     
-    public init(_ content: @escaping (EdgeInsets) -> Content) {
+    public init(@ViewBuilder _ content: @MainActor @escaping (EdgeInsets) -> Content) {
         self.content = content
     }
     
+    private func calculate() -> EdgeInsets {
+        EdgeInsets(
+            top: frame.minY <= sceneInsets.top ? sceneInsets.top : 0,
+            leading: frame.minX <= sceneInsets.leading ? sceneInsets.leading : 0,
+            bottom: frame.maxY >= sceneSize.height ? sceneInsets.bottom : 0,
+            trailing: frame.maxX >= sceneSize.width - sceneInsets.trailing ? sceneInsets.trailing : 0
+        )
+    }
+    
     public var body: some View {
-        GeometryReader { proxy in
-            let frame = proxy.frame(in: .global)
-            
-            content(EdgeInsets(
-                top: frame.minY <= sceneInsets.top ? sceneInsets.top : 0,
-                leading: frame.minX <= sceneInsets.leading ? sceneInsets.leading : 0,
-                bottom: frame.maxY >= sceneSize.height - sceneInsets.bottom ? sceneInsets.bottom : 0,
-                trailing: frame.maxX >= sceneSize.width - sceneInsets.trailing ? sceneInsets.trailing : 0
-            ))
-        }
+        content(disabledInsets ?? calculate())
+            .onGeometryChangePolyfill(of: { $0.frame(in: .global) }){ frame = $0 }
+            .onChangePolyfill(of: disabled, initial: true){
+                disabledInsets = disabled ? calculate() : nil
+            }
     }
     
 }
@@ -52,7 +59,34 @@ public extension View {
                 .padding(.top, edges.contains(.top) ? inset.top : 0)
                 .padding(.trailing, edges.contains(.trailing) ? inset.trailing : 0)
                 .padding(.bottom, edges.contains(.bottom) ? inset.bottom : 0)
+                .animation(.fastSpringInterpolating, value: inset)
         }
+    }
+    
+}
+
+
+struct DisableInsetReaderKey: EnvironmentKey {
+    
+    static var defaultValue: Bool { false }
+    
+}
+
+
+extension EnvironmentValues {
+    
+    var insetReadingDisabled: Bool {
+        get { self[DisableInsetReaderKey.self] }
+        set { self[DisableInsetReaderKey.self] = newValue }
+    }
+    
+}
+
+
+public extension View {
+    
+    nonisolated func disableInsetReading(_ disabled: Bool = true) -> some View {
+        environment(\.insetReadingDisabled, disabled)
     }
     
 }
