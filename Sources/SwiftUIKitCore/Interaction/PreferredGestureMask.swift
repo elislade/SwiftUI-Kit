baa43039737d49f493de82f06fa98e3fa364f9a5
Eli Slade <elislade@gmail.com>
2025-01-04 00:39:28 -0800

    Added PreferredGestureMask

---
+++ Sources/SwiftUIKitCore/Interaction/PreferredGestureMask.swift
@@ -0,0 +1,28 @@
+import SwiftUI
+
+
+struct PreferredGestureMask: PreferenceKey {
+    
+    typealias Value = GestureMask?
+    
+    static func reduce(value: inout GestureMask?, nextValue: () -> GestureMask?) {
+        if let next = nextValue() {
+            value = next
+        }
+    }
+    
+}
+
+public extension View {
+    
+    /// Ask for preferred gesture mask of some parent gesture
+    nonisolated func preferredParentGestureMask(_ mask: GestureMask?) -> some View {
+        preference(key: PreferredGestureMask.self, value: mask)
+    }
+    
+    /// Listen for child requests of mask change
+    nonisolated func onPreferredGestureMaskChange(perform action: @escaping @Sendable (GestureMask?) -> Void) -> some View {
+        onPreferenceChange(PreferredGestureMask.self, perform: action)
+    }
+    
+}
