6aa542cfb7b7d26f2bdfed5fe0bfef093825729e
Eli Slade <elislade@gmail.com>
2025-02-04 14:09:17 -0800

    Added WindowGeometryReading

---
+++ Sources/SwiftUIKitCore/Window/WindowGeometry.swift
@@ -0,0 +1,41 @@
+import SwiftUI
+import Combine
+
+
+public extension View {
+    
+    func onWindowGeometryChange<V: Equatable>(
+        type: V.Type = V.self,
+        transform: @escaping(CGRect) -> V,
+        perform action: @escaping(V) -> Void
+    ) -> some View {
+        modifier(WindowGeometryModifier(transform: transform, action: action))
+    }
+    
+}
+
+
+struct WindowGeometryModifier<V: Equatable>: ViewModifier {
+    
+    let transform: (CGRect) -> V
+    let action: (V) -> Void
+    
+    @State private var publisher: AnyPublisher<V, Never>?
+    
+    func body(content: Content) -> some View {
+        content.windowReference {
+            publisher = $0.publisher(for: \.frame)
+                .map{ transform($0) }
+                .removeDuplicates()
+                .eraseToAnyPublisher()
+        }
+        .overlay {
+            if let publisher {
+                Color.clear.onReceive(publisher) {
+                    action($0)
+                }
+            }
+        }
+    }
+    
+}
