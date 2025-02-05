f79978396316eebe975f8b36ae9e0384fc1ef8af
Eli Slade <elislade@gmail.com>
2025-02-05 15:19:37 -0800

    Improved Menu Accessibility.

--- Sources/SwiftUIMenu/Menu.swift
+++ Sources/SwiftUIMenu/Menu.swift
@@ -49,13 +49,13 @@ public struct Menu<Label: View, Content: View>: View {
                 .padding(.init(state.edge))
                 .shadow(color: .black.opacity(0.1), radius: 30, y: 20)
                 .windowInteractionEffects([.scale(anchor: state.anchor)])
-                .presentationBackground(.touchChangeDismiss){ Color.clear }
+                .presentationBackground(.touchEndedDismiss){ Color.clear }
                 .modifier(SubmenuPresentationContext())
                 .environment(\.menuBackground, menuBackground)
-                //.transitions((.move(edge: .bottom) + .opacity).animation(.bouncy.speed(1.6)))
-                //.transitions(.scale.animation(.fastSpringInteractive))
             }
-            
+            .accessibilityRepresentation {
+                content()
+            }
         }
     }
     
