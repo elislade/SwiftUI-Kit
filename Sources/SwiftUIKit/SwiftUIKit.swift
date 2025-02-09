66ef1d4f88f5eb8b80426302d83b6d56fb845379
Eli Slade <elislade@gmail.com>
2025-02-08 18:31:45 -0800

    Added SwiftUIKit typealias comments.

--- Sources/SwiftUIKit/SwiftUIKit.swift
+++ Sources/SwiftUIKit/SwiftUIKit.swift
@@ -7,6 +7,11 @@
 @_exported import SwiftUILayout
 @_exported import SwiftUIControls
 
+
+// Override SwiftUI Types with SwiftUIKit types.
+// These types share some of the same initialization signatures as SwiftUI's versions.
+// To use the SwiftUI disambiguation you need to explicitly prefix it's type with the SwiftUI namespace.
+// Eg. SwiftUI.Menu() will use the SwiftUI version over the kit implimentation.
 public typealias Menu = SwiftUIMenu.Menu
 public typealias Slider = SwiftUIControls.Slider
 public typealias TextField = SwiftUIControls.TextField
