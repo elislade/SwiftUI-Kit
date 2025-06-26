094da6a1d5440790867dd14efcee79ee43d6942c
Eli Slade <elislade@gmail.com>
2025-06-25 18:20:20 -0700

    Added: InlineAppStorage wrapper for accessing Codable stored values inline.

---
+++ Sources/SwiftUIKitCore/Inline/InlineAppStorage.swift
@@ -0,0 +1,33 @@
+import SwiftUI
+
+
+public struct InlineAppStorage<Value : Codable & Equatable, Content: View>: View  {
+    
+    let value: State<Value>
+    let storage: AppStorage<Data?>
+    let content: (Binding<Value>) -> Content
+    
+    public init(
+        _ key: String,
+        initial: Value,
+        store: UserDefaults? = nil,
+        @ViewBuilder content: @escaping (Binding<Value>) -> Content
+    ) {
+        let sotrageData = AppStorage<Data?>(key, store: store)
+        if let data = sotrageData.wrappedValue, let decoded = try? JSONDecoder().decode(Value.self, from: data) {
+            self.value = .init(initialValue: decoded)
+        } else {
+            self.value = .init(initialValue: initial)
+        }
+        self.storage = sotrageData
+        self.content = content
+    }
+    
+    public var body: some View {
+        content(value.projectedValue)
+            .onChangePolyfill(of: value.wrappedValue){
+                storage.wrappedValue = try? JSONEncoder().encode(value.wrappedValue)
+            }
+    }
+    
+}
