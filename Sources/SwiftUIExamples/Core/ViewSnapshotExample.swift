8c139c8318db57ac5e54d5ec9360c0ce91d4fefc
Eli Slade <elislade@gmail.com>
2025-07-27 10:36:25 -0700

    Updated: ViewSnapshotExample incorrectly got commited with not the latest version, but the one when the file was moved/created.  + This seems to be a bug with Xcode git integration.

--- Sources/SwiftUIExamples/Core/ViewSnapshotExample.swift
+++ Sources/SwiftUIExamples/Core/ViewSnapshotExample.swift
@@ -1,46 +1,108 @@
 import SwiftUIKit
 
 
-struct SnapshotTest: View {
+public struct ViewSnapshotExample: View {
     
-    @State private var snapshot: AnyView?
+    struct IDView {
+        let id: UUID = .init()
+        let view: AnyView
+    }
+    
+    @State private var snapshot: IDView?
     @State private var signal = true
-
-    var body: some View {
-        VStack(spacing: 0) {
-            ZStack {
-                Color.clear
-                snapshot
+    @State private var showBackgroundBlurArtifacting = true
+    
+    public init(){}
+    
+    public var body: some View {
+        VStack(spacing: 10) {
+            Color.secondary.opacity(0.1).overlay {
+                if let snapshot {
+                    snapshot.view
+                        .allowsHitTesting(false)
+                        .transition(
+                            .asymmetric(
+                                insertion: .offset([0,400]),
+                                removal: .scale(scale: 0, anchor: .top) + .opacity + .blur(radius: 20)
+                            )
+                            .animation(.bouncy)
+                        )
+                        .id(snapshot.id)
+                        .zIndex(1)
+                }
             }
-            
-            Divider()
+            .zIndex(2)
             
             ZStack {
-                Circle().fill(.red)
+                if showBackgroundBlurArtifacting {
+                    Circle()
+                        .fill(.red)
+                        .frame(width: 140, height: 140)
+                }
                 
                 ScrollView {
-                    VStack(spacing: 10) {
-                        ForEach(0..<50){ i in
-                            Color.random.frame(height: 60)
+                    VStack(spacing: 50) {
+                        ForEach(0..<9){ i in
+                            Rectangle()
+                                .fill(Color.random)
+                                .frame(height: 100)
                         }
                     }
                 }
-                .background(.bar)
+                .background(.regularMaterial)
             }
-            .viewSnapshot(for: signal){
-                snapshot = $0
+            .viewSnapshot(for: signal, initial: true){ v in
+                snapshot = .init(view: v)
             }
-            
-            Button("Capture") {
-                signal.toggle()
+            .id(showBackgroundBlurArtifacting)
+        }
+        .overlay(alignment: .center){
+            Button { signal.toggle() } label: {
+                Label{ Text("Capture") } icon: {
+                    Image(systemName: "camera.aperture")
+                        .resizable()
+                        .scaledToFit()
+                        .opacity(0.8)
+                        .padding(20)
+                        .foregroundStyle(.white)
+                        .symbolEffectBounce(value: signal)
+                        .background{
+                            Circle()
+                                .fill(.white)
+                                .shadow(radius: 10, y: 10)
+                            Circle()
+                                .fill(.red)
+                                .padding(5)
+                        }
+                }
+            }
+            .frame(height: 80)
+        }
+        .ignoresSafeArea(edges: .bottom)
+        .safeAreaInset(edge: .top, spacing: 0){
+            VStack {
+                ExampleTitle("View Snapshot")
+                
+                #if !os(watchOS)
+                Toggle(isOn: $showBackgroundBlurArtifacting){
+                    Text("BG Blur Artifacting")
+                        .font(.exampleParameterTitle)
+                        .minimumScaleFactor(0.8)
+                }
+                #endif
             }
             .padding()
+            .background(.regularMaterial)
         }
+        .buttonStyle(.plain)
+        .labelStyle(.iconOnly)
+        .toggleStyle(.swiftUIKitSwitch)
     }
     
 }
 
 
 #Preview {
-    SnapshotTest()
+    ViewSnapshotExample()
+        .previewSize()
 }
