00a56a734b308a5890b9533d6fda65d4770da02f
Eli Slade <elislade@gmail.com>
2025-07-27 10:17:09 -0700

    Added: FrozenState indicates to other modifier and views that they should stop their usual behaviour.

---
+++ Sources/SwiftUIKitCore/FrozenState/FrozenState.swift
@@ -0,0 +1,34 @@
+import SwiftUI
+
+/// FrozenState is an experimental way to improve performance by telling other views and modifiers to stop their usual behaviour while remaining in the graph tree in the state you last froze them in.
+/// - Note : Should be used if a view is known to become active/thawed again after being disabled/frozen but you don't want to incure the cost of view setup/teardown restoration, like in complicated view hirarchies.
+public enum FrozenState: Int, Hashable, Sendable {
+    case thawed
+    case frozen
+    case frozenInvisible
+    
+    public var isThawed: Bool { self == .thawed }
+    public var isFrozen: Bool { self != .thawed }
+}
+
+
+public extension EnvironmentValues {
+    
+    @Entry var frozenState: FrozenState = .thawed
+    
+}
+
+
+public extension View {
+    
+    func frozen(_ newState: FrozenState = .frozen) -> some View {
+        modifier(FrozenStateModifier())
+            .transformEnvironment(\.frozenState){ state in
+                // Only override parent value it's set to default thawed
+                if state.isThawed {
+                    state = newState
+                }
+            }
+    }
+    
+}
---
+++ Sources/SwiftUIKitCore/FrozenState/FrozenStateModifier.swift
@@ -0,0 +1,33 @@
+import SwiftUI
+
+
+struct FrozenStateModifier: ViewModifier {
+    
+    @State private var frozenSize: CGSize?
+    @Environment(\.frozenState) private var state
+    
+    func body(content: Content) -> some View {
+        content
+            .background{
+                GeometryReader{ proxy in
+                    Color.clear.onChangePolyfill(of: state.isFrozen, initial: true){
+                        if state.isFrozen {
+                            frozenSize = proxy.size
+                        } else {
+                            frozenSize = nil
+                        }
+                    }
+                }
+            }
+            .allowsHitTesting(state.isThawed)
+            .opacity(state == .frozenInvisible ? 0 : 1)
+            .accessibilityHidden(state.isFrozen)
+            .frame(
+                width: frozenSize?.width,
+                height: frozenSize?.height
+            )
+            .disableAnimations(state == .frozenInvisible)
+            .scrollOffsetDisabled(state.isFrozen)
+    }
+    
+}
