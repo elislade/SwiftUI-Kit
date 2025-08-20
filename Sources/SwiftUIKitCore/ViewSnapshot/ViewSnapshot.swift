877f99d859e493bce58045e0419fe3d7d19087ce
Eli Slade <elislade@gmail.com>
2025-08-19 19:34:35 -0700

    Updated: PresentationSnapshot now has an environment value that views can use to know if they are in one.

--- Sources/SwiftUIKitCore/ViewSnapshot/ViewSnapshot.swift
+++ Sources/SwiftUIKitCore/ViewSnapshot/ViewSnapshot.swift
@@ -8,7 +8,7 @@ public extension View {
     /// - NOTE: Capture rate may be ratelimited. The action may not be called for every value change.
     /// - Parameters:
     ///   - value: An equatable value to use for deciding when to capture.
-    ///   - initial: Bool declaring wheater to capture image on first value pass with no changes. Defaults to true.
+    ///   - initial: Bool declaring whether to capture image on first value pass with no changes. Defaults to true.
     ///   - action: A closure that gets called with the Image that was captured.
     /// - Returns: A Modified view
     nonisolated func viewSnapshot<V: Equatable>(
@@ -17,11 +17,18 @@ public extension View {
         perform action: @MainActor @escaping (AnyView) -> Void
     ) -> some View {
         ViewSnapshoter(
-            content: self,
+            content: self.environment(\.isSnapshot, true),
             value: value,
             initial: initial,
             action: action
         )
     }
+    
+}
+
+
+extension EnvironmentValues {
+    
+    @Entry public internal(set) var isSnapshot: Bool = false
     
 }
