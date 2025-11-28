dc27fe8c4c9e148b746828e2fd3d3691b8c1ea5f
Eli Slade <elislade@gmail.com>
2025-11-27 20:40:46 -0800

    Updated: Simlified POSIX machine Unicode parsing.

--- Sources/SwiftUIKitCore/Device/DeviceInfo.swift
+++ Sources/SwiftUIKitCore/Device/DeviceInfo.swift
@@ -11,10 +11,12 @@ final public class Device {
     public private(set) lazy var modelName: String = {
         var systemInfo = utsname()
         uname(&systemInfo)
-        let machineMirror = Mirror(reflecting: systemInfo.machine)
-        return machineMirror.children.reduce("") { identifier, element in
-            guard let value = element.value as? Int8, value != 0 else { return identifier }
-            return identifier + String(UnicodeScalar(UInt8(value)))
+        return withUnsafeBytes(of: &systemInfo.machine){ pointer in
+            pointer.bindMemory(to: UInt8.self).reduce(into: "") { identifier, element in
+                if element != 0 {
+                    identifier += String(UnicodeScalar(element))
+                }
+            }
         }
     }()
     
