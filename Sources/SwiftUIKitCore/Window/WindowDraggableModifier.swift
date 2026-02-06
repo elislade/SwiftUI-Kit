6c63fe1f1ac63745e50031fee91038064ad915eb
Eli Slade <elislade@gmail.com>
2026-02-05 17:50:34 -0800

    Updated: WindowDraggable can now work in isolation without a root window context on macOS.

--- Sources/SwiftUIKitCore/Window/WindowDraggableModifier.swift
+++ Sources/SwiftUIKitCore/Window/WindowDraggableModifier.swift
@@ -1,19 +1,41 @@
 import SwiftUI
 
-#if canImport(AppKit)
+#if os(macOS)
 
-struct WindowDraggableModifier: ViewModifier {
+struct WindowDraggableModifier {
     
     @Environment(\.performWindowAction) private var performWindowAction
+    @State private var action: () -> Void = { }
     
     let enabled: Bool
+    
+}
+
+extension WindowDraggableModifier: ViewModifier {
     
     func body(content: Content) -> some View {
         content
             .contentShape(Rectangle())
             .onLongPressGesture(minimumDuration: 0.05){
                 if enabled {
-                    performWindowAction(.startMove)
+                    action()
+                }
+            }
+            .windowReference{ window in
+                action = {
+                    window.trackEvents(
+                        matching: [.leftMouseDragged, .leftMouseUp],
+                        timeout: 0.2,
+                        mode: .eventTracking
+                    ){ evt, stop in
+                        if let evt {
+                            if evt.type == .leftMouseUp {
+                                stop.pointee = true
+                            } else {
+                                evt.window?.performDrag(with: evt)
+                            }
+                        }
+                    }
                 }
             }
     }
@@ -22,14 +44,14 @@ struct WindowDraggableModifier: ViewModifier {
 
 #endif
 
-public extension View {
+extension View {
     
-#if canImport(AppKit)
-    func windowDraggable(enabled: Bool = true) -> some View {
+#if os(macOS)
+    nonisolated public func windowDraggable(enabled: Bool = true) -> some View {
         modifier(WindowDraggableModifier(enabled: enabled))
     }
 #else
-    func windowDraggable(enabled: Bool = true) -> Self {
+    nonisolated public func windowDraggable(enabled: Bool = true) -> Self {
         self
     }
 #endif
