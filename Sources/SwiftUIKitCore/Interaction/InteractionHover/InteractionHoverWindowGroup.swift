992cf75d13b6b73ac04b537081aba335b2642fd1
Eli Slade <elislade@gmail.com>
2026-04-08 00:35:00 -0700

    Fixed: window drag and interaction hover on watchOS not responding properly.

--- Sources/SwiftUIKitCore/Interaction/InteractionHover/InteractionHoverWindowGroup.swift
+++ Sources/SwiftUIKitCore/Interaction/InteractionHover/InteractionHoverWindowGroup.swift
@@ -7,6 +7,8 @@ struct InteractionHoverWindowGroup {
     
     @State private var previousID: UUID?
     @State private var enabled = false
+    @State private var elements: [InteractionHoverElement] = []
+    @State private var frame: CGRect = .zero
     
     let hapticsEnabled: Bool
     let hitTestOnStart: Bool
@@ -20,74 +22,71 @@ extension InteractionHoverWindowGroup: ViewModifier {
     func body(content: Content) -> some View {
         content
             .environment(\.interactionGroupCoordinateSpace, .global)
-            .overlayPreferenceValue(InteractionHoverElementPreference.self){ elements in
-                GeometryReader{ proxy in
-                    if !elements.isEmpty && !isBeingPresentedOn {
-                        Color.clear.onWindowDrag{ evt in
-                            guard let location = evt.locations.first else { return }
-                            
-                            if hitTestOnStart {
-                                if evt.phase == .began {
-                                    enabled = proxy.frame(in: .global).contains(location)
-                                    if enabled {
-                                        action(.init(date: Date(), phase: .started))
-                                    }
-                                }
-                            } else {
-                                if !enabled {
-                                    action(.init(date: Date(), phase: .started))
-                                }
-                                enabled = true
-                            }
-                            
-                            defer {
-                                if evt.phase == .ended {
-                                    enabled = false
-                                }
-                            }
-                            
-                            guard enabled else { return }
-                            
-                            if let index = elements.firstIndex(where: { $0.coordinateSpaceBounds.contains(location) }) {
-                                let id = elements[index].id
-                                
-                                if evt.phase == .ended {
-                                    if
-                                        id != previousID,
-                                        let previousIndex = elements.firstIndex(where: { $0.id == previousID })
-                                    {
-                                        elements[previousIndex](.left)
-                                    }
-                                    
-                                    elements[index](.ended)
-                                    previousID = nil
-                                    action(.init(date: Date(), phase: .ended))
-                                } else {
-                                    if previousID != id {
-                                        if let previousIndex = elements.firstIndex(where: { $0.id == previousID }) {
-                                            elements[previousIndex](.left)
-                                            action(.init(date: Date(), phase: .exited))
-                                        }
-                                        
-                                        elements[index](.entered)
-                                        action(.init(date: Date(), phase: .entered))
-                                        previousID = id
-                                    }
-                                }
-                            } else if let previousIndex = elements.firstIndex(where: { $0.id == previousID }) {
-                                elements[previousIndex](.left)
-                                self.previousID = nil
-                                action(.init(date: Date(), phase: .exited))
-                            }
-                        }
-                        .onDisappear{
-                            previousID = nil
-                            enabled = false
+            .preferenceChangeConsumer(InteractionHoverElementPreference.self){ elements = $0 }
+            .onWindowDrag{ evt in
+                guard
+                    !elements.isEmpty && !isBeingPresentedOn,
+                    let location = evt.locations.first
+                else {
+                    previousID = nil
+                    enabled = false
+                    return
+                }
+
+                if hitTestOnStart {
+                    if evt.phase == .began {
+                        enabled = frame.contains(location)
+                        if enabled {
+                            action(.init(date: Date(), phase: .started))
                         }
                     }
+                } else {
+                    if !enabled {
+                        action(.init(date: Date(), phase: .started))
+                    }
+                    enabled = true
+                }
+
+                defer {
+                    if evt.phase == .ended {
+                        enabled = false
+                    }
+                }
+
+                guard enabled else { return }
+
+                if let index = elements.firstIndex(where: { $0.coordinateSpaceBounds.contains(location) }) {
+                    let id = elements[index].id
+
+                    if evt.phase == .ended {
+                        if
+                            id != previousID,
+                            let previousIndex = elements.firstIndex(where: { $0.id == previousID })
+                        {
+                            elements[previousIndex](.left)
+                        }
+
+                        elements[index](.ended)
+                        previousID = nil
+                        action(.init(date: Date(), phase: .ended))
+                    } else {
+                        if previousID != id {
+                            if let previousIndex = elements.firstIndex(where: { $0.id == previousID }) {
+                                elements[previousIndex](.left)
+                                action(.init(date: Date(), phase: .exited))
+                            }
+
+                            elements[index](.entered)
+                            action(.init(date: Date(), phase: .entered))
+                            previousID = id
+                        }
+                    }
+                } else if let previousIndex = elements.firstIndex(where: { $0.id == previousID }) {
+                    elements[previousIndex](.left)
+                    self.previousID = nil
+                    action(.init(date: Date(), phase: .exited))
                 }
             }
-            .resetPreference(InteractionHoverElementPreference.self)
             .background{
                 if hapticsEnabled {
                     Color.clear.sensoryFeedbackPolyfill(value: previousID)
--- Sources/SwiftUIKitCore/Window/Events/WindowDrag/WindowDrag.swift
+++ Sources/SwiftUIKitCore/Window/Events/WindowDrag/WindowDrag.swift
@@ -1,27 +1,10 @@
 import SwiftUI
 
 extension View {
-    
-    #if os(watchOS)
-    
-    public nonisolated func onWindowDrag(perform action: @escaping (WindowDragEvent) -> Void) -> some View {
-        InlineEnvironmentValue(\.windowDragEnabled){ enabled in
-            contentShape(Rectangle()).simultaneousGesture(
-                DragGesture(minimumDistance: 0, coordinateSpace: .global)
-                    .onChanged{ action(.init(phase: .changed, locations: [$0.location])) }
-                    .onEnded{ action(.init(phase: .ended, locations: [$0.location])) },
-                including: enabled ? .all : .none
-            )
-        }
-    }
-    
-    #else
-    
+
     public nonisolated func onWindowDrag(perform action: @escaping (WindowDragEvent) -> Void) -> some View {
         modifier(WindowDragModifier(action: action))
     }
-    
-    #endif
     
     public nonisolated func windowDragDisabled(_ disabled: Bool = true) -> some View {
         transformEnvironment(\.windowDragEnabled){ isEnabled in
